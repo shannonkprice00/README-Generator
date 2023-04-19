0b2d21ba5bf22d9ba088bc15b9d8cb9f67d21887
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-04-19 12:58:08 -0500

    Added code for MVP

--- index.js
+++ index.js
@@ -1,13 +1,88 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Write any installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Write usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Write contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Write test instructions:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license is used for your project?',
+        choices: [
+            'None',
+            'Apache License 2.0', 
+            'GNU General Public License v3.0',
+            'MIT License',
+            'BSD 2-Clause "Simplified" License',
+            'BSD 3-Clause "New" or "Revised" License',
+            'Boost Software License 1.0',
+            'Eclipse Public License 2.0',
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License v2.0',
+            'GNU Lesser General Public License v2.1',
+            'Mozilla Public License 2.0',
+            'The Unlicense',
+        ]
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) => {
+        err ? console.log(err) : console.log('Successfully created README.md!')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        const fileName = "README.md";
+        writeToFile(fileName, data);
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,121 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+  if (license === "None") {
+    badge = "";
+  } else if (license === "Apache License 2.0") {
+    badge =
+      "[![License: Apache](https://img.shields.io/badge/License-Apache-blue.svg)]";
+  } else if (license === "GNU General Public License v3.0") {
+    badge =
+      "[![License: GNU General Public](https://img.shields.io/badge/License-GNU%20General%20Public-blue.svg)]";
+  } else if (license === "MIT License") {
+    badge =
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)]";
+  } else if (license === 'BSD 2-Clause "Simplified" License') {
+    badge =
+      "[![License: BSD-2-Clause](https://img.shields.io/badge/License-BSD%202%20Clause-blue.svg)]";
+  } else if (license === 'BSD 3-Clause "New" or "Revised" License') {
+    badge =
+      "[![License: BSD-3-Clause](https://img.shields.io/badge/License-BSD%203%20Clause-blue.svg)]";
+  } else if (license === "Boost Software License 1.0") {
+    badge =
+      "[![License: BSL-1](https://img.shields.io/badge/License-BSL%201-blue.svg)]";
+  } else if (license === "Eclipse Public License 2.0") {
+    badge =
+      "[![License: Eclipse Public License 2.0](https://img.shields.io/badge/License-Eclipse%20Public%20License%202.0-blue.svg)]";
+  } else if (license === "GNU Affero General Public License v3.0") {
+    badge =
+      "[![License: AGPL-3.0-only](https://img.shields.io/badge/License-AGPL%203.0%20only-blue.svg)]";
+  } else if (license === "GNU General Public License v2.0") {
+    badge =
+      "[![License: GPL-2.0](https://img.shields.io/badge/License-GPL%202.0-blue.svg)]";
+  } else if (license === "GNU Lesser General Public License v2.1") {
+    badge =
+      "[![License: LGPL-2.1](https://img.shields.io/badge/License-LGPL%202.1-blue.svg)]";
+  } else if (license === "Mozilla Public License 2.0") {
+    badge =
+      "[![License: MPL-2.0](https://img.shields.io/badge/License-MPL%202.0-blue.svg)]";
+  } else if (license === "The Unlicense") {
+    badge =
+      "[![License: Unlicense](https://img.shields.io/badge/License-Unlicense-blue.svg)]";
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link = "";
+  if (license === "None") {
+    link = "";
+  } else if (license === "Apache License 2.0") {
+    link = "(https://opensource.org/license/apache-2-0/)";
+  } else if (license === "GNU General Public License v3.0") {
+    link = "(https://opensource.org/license/gpl-3-0/)";
+  } else if (license === "MIT License") {
+    link = "(https://opensource.org/license/mit/)";
+  } else if (license === 'BSD 2-Clause "Simplified" License') {
+    link = "(https://opensource.org/license/bsd-2-clause/)";
+  } else if (license === 'BSD 3-Clause "New" or "Revised" License') {
+    link = "(https://opensource.org/license/bsd-3-clause/)";
+  } else if (license === "Boost Software License 1.0") {
+    link = "(https://opensource.org/license/bsl1-0-html/)";
+  } else if (license === "Eclipse Public License 2.0") {
+    link = "(https://opensource.org/license/epl-2-0/)";
+  } else if (license === "GNU Affero General Public License v3.0") {
+    link = "(https://opensource.org/license/agpl-v3/)";
+  } else if (license === "GNU General Public License v2.0") {
+    link = "(https://opensource.org/license/gpl-2-0/)";
+  } else if (license === "GNU Lesser General Public License v2.1") {
+    link = "(https://opensource.org/license/lgpl-2-1/)";
+  } else if (license === "Mozilla Public License 2.0") {
+    link = "(https://opensource.org/license/mpl-2-0/)";
+  } else if (license === "The Unlicense") {
+    link = "(https://opensource.org/license/unlicense/)";
+  }
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = "";
+  if (license === "None") {
+    licenseSection = "";
+  } else {
+    licenseSection = `${renderLicenseBadge(license)}${renderLicenseLink(license)}`;
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+${renderLicenseSection(data.license)}
+## Description 
+${data.description}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${data.license}
+## Contributing
+${data.contribution}
+## Tests
+${data.test}
+## Questions
+GitHub Profile: [github.com/${data.github}](https://github.com/${data.github})<br>
+For further questions, you can reach me at ${data.email}
 `;
 }
 
